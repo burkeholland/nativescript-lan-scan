13ea81d9471e0133a969c6864bc21f61038d7f20
Burke Holland <burkeholland@gmail.com>
2017-02-02 19:30:55 -0600

    Freshening up the demo

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,12 +1,15 @@
 import {Observable} from 'data/observable';
 import { ObservableArray } from 'data/observable-array';
-import { LanScan, FoundDeviceEventData, DeviceInfo } from 'nativescript-lan-scan';
+import { LanScan, FoundDeviceEventData, DeviceInfo, PingProgressEventData, PingProgress } from 'nativescript-lan-scan';
 
 export class HelloWorldModel extends Observable {
 
     private _lanScan: LanScan;
 
     public devices: ObservableArray<DeviceInfo>;
+    public status: string = "Scan Test";
+    public max: number = 254;
+    public progress: number = 0;
 
     constructor() {
         super();
@@ -18,10 +21,20 @@ export class HelloWorldModel extends Observable {
         this._lanScan.on(LanScan.foundNewDeviceEvent, (args: FoundDeviceEventData) => {
             this.devices.push(args.deviceInfo);
         });
+
+        this._lanScan.on(LanScan.progressPingedEvent, (args: PingProgressEventData) => {
+            this.set('max', args.pingProgress.overallHosts);
+            this.set('progress', args.pingProgress.pingedHosts);
+        });
+
+        this._lanScan.on(LanScan.scanningFinishedEvent, (args) => {
+            this.set('status', 'Finished');
+        });
     }
 
     onTap() {
         console.log('starting the lan scanner');
         this._lanScan.start();
+        this.set('status', 'Scanning');
     }
 }
